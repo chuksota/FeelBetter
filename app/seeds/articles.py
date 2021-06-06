02b5785968774e6d0d19a2e7c3af5031db80e9df
Chuks <chuksota@gmail.com>
2021-06-06 17:21:07 -0500

    working on the article card

--- app/seeds/articles.py
+++ app/seeds/articles.py
@@ -3,9 +3,11 @@ from app.models import db, Article
 
 def seed_articles():
   article1 = Article(
-     title= "Test Article",
-     description= "This is a test article to make sure I can get the information necessary.",
-     sources_id = 1
+     title= "A New Mental Health Crisis Is Raging in Gaza",
+     description="Have you ever seen a six-month old baby with exaggerated startle response?” One of my colleagues who works on our telephone counseling service was calling me for advice on how to respond to several distraught mothers asking her how to help their babies who had started showing such distressing symptoms of trauma during the recent bombing. Our telephone service was back and responding to callers on the third day of the attacks on Gaza, though of course with certain difficulties. The question took me back 20 years to when I was a young resident in the pediatric department at Nasser hospital in Khan Younis, Gaza’s second biggest city, in the southern part of the Gaza Strip. Then, my plan was to become a pediatrician. The hospital, on the western side of the city was not far from the Israeli settlements. Often in the middle of the night I used to receive mothers arriving in the pediatric emergency department with tiny children who had started screaming with no clear reason. Physical examination mostly revealed nothing abnormal. Perhaps this was the trigger that made me train to become a psychiatrist. During those nights, you could often hear shooting from inside the Israeli settlement’s high fortifications, with the bullets mostly ending in the walls of the Palestinian homes and other buildings that faced the settlements. That was the common experience we adults were used to, and of course something that children, even the very youngest, also had to live with. Thinking about those mothers and babies, I then asked myself about the likely psychological consequences of this 11-day offensive on the people of the Gaza Strip, and how it is going to be different from 2014’s Gaza war which lasted for seven weeks through July and August, including a ground invasion into Gaza. There were then 2,251 Palestinians killed and 11,000 wounded.",
+     sources_id = 1,
+     image_url='https://feelbetterbucket.s3.ca-central-1.amazonaws.com/1A55289F-8B96-47D6-A31498EAB161911D_source.jpg',
+     website_link='https://www.scientificamerican.com/article/a-new-mental-health-crisis-is-raging-in-gaza1/'
   )
   db.session.add(article1)
   db.session.commit()
--- react-app/src/components/Articles.js
+++ react-app/src/components/Articles.js
@@ -3,25 +3,98 @@ import { useDispatch, useSelector } from 'react-redux'
 import {useParams} from 'react-router-dom'
 import ProfileDrawer from './profileDrawer/ProfileDrawer'
 import {loadA} from '../store/source'
+import { makeStyles } from '@material-ui/core/styles';
+import clsx from 'clsx';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Collapse from '@material-ui/core/Collapse';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import ShareIcon from '@material-ui/icons/Share';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    maxWidth: 350,
+  },
+  media: {
+    height: 0,
+    paddingTop: '56.25%', // 16:9
+  },
+  expand: {
+    transform: 'rotate(0deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+      duration: theme.transitions.duration.shortest,
+    }),
+  },
+  expandOpen: {
+    transform: 'rotate(180deg)',
+  },
+}));
 const Source = () => {
+  const classes = useStyles();
+  const [expanded, setExpanded] = React.useState(false);
   const dispatch = useDispatch()
   const feeds = useSelector(state=> state.session.user.feeds)
   const {id} =  useParams()
   const articles = useSelector(state=> state.sources)
-  console.log(articles)
+
   useEffect(()=>{
     dispatch(loadA(id))
   },[dispatch, id])
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+
   return (
-    <>
-    <ProfileDrawer feeder={feeds}/>
-    <div className='right_view_area'>
-      <div>{articles[1]?.title}</div>
-      <div>{articles[1]?.description}</div>
+  <>
+    // <ProfileDrawer feeder={feeds}/>
+    <div className="right_view_area">
+    <Card className={classes.root}>
+      <CardHeader
+        title={articles[1]?.title}
+        subheader="September 14, 2016"
+      />
+      <CardMedia
+        className={classes.media}
+        image={articles[1]?.image_url}
+        title="Paella dish"
+      />
+      <CardContent>
+        <Typography variant="body2" color="textSecondary" component="p">
+        Recent bombings by Israel have caused more than just physical trauma
+        </Typography>
+      </CardContent>
+      <CardActions disableSpacing>
+        <IconButton
+          className={clsx(classes.expand, {
+            [classes.expandOpen]: expanded,
+          })}
+          onClick={handleExpandClick}
+          aria-expanded={expanded}
+          aria-label="show more"
+        >
+          <ExpandMoreIcon />
+        </IconButton>
+      </CardActions>
+      <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <CardContent>
+          <Typography paragraph>
+            {articles[1]?.description}
+          </Typography>
+        </CardContent>
+      </Collapse>
+    </Card>
     </div>
     </>
-  )
+  );
 }
 
 export default Source
